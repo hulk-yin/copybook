e8963b9f9039797f5801c89e9b7c1c027c5b531b
hulk-yin <yinkehao@163.com>
2020-02-23 05:35:05 +0000

    Deploying to master from master 4ddc48b384ef8e5fd45a73f3e209f46f925c5a12

---
+++ precache-manifest.fb3423cce1bec8dd261c8edfb555e21b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ccdb033b1078184a5756f73ff387cc",
+    "url": "/copybook/index.html"
+  },
+  {
+    "revision": "ada43bc10881a07a401d",
+    "url": "/copybook/static/css/main.19d1e137.chunk.css"
+  },
+  {
+    "revision": "18201c81588950810b39",
+    "url": "/copybook/static/js/2.50402c31.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/copybook/static/js/2.50402c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada43bc10881a07a401d",
+    "url": "/copybook/static/js/main.96db4a30.chunk.js"
+  },
+  {
+    "revision": "3fe11780c419f329546a",
+    "url": "/copybook/static/js/runtime-main.5c075901.js"
+  },
+  {
+    "revision": "439f3503db71d84f6ed37645cca025da",
+    "url": "/copybook/static/media/FZKTJW.439f3503.TTF"
+  },
+  {
+    "revision": "ca994a92615a60a478f26a452387406a",
+    "url": "/copybook/static/media/FZXKTJW.ca994a92.TTF"
+  }
+]);
