14b8bcee50436a2c8218accb78fd10077f411d88
kehao.ykh <kehao.ykh@alibaba-inc.com>
2020-02-23 12:40:09 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
-
+import Matts from './matts';
 function App() {
+  const [str, setWords] = useState("你好！");
+  const [size, setSize] = useState(200);
+  const [type, setType] = useState("tian");
+  const words = str.split("");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button
+            onClick={() => setSize(Math.max(30, size - 10))}
+          >-</button>{size}<button
+            onClick={() => setSize(Math.min(300, size + 10))}
+          >+</button>
+          &nbsp;
+          &nbsp;
+          <button
+            onClick={() =>{
+              const s= str;
+              setWords("");
+              setType('tian')
+              requestAnimationFrame(()=>{
+                setWords(s);
+              })
+            }}
+          >田字格</button>
+          <button
+            onClick={() => {
+              const s= str;
+              setWords("");
+              setType('mi')
+              requestAnimationFrame(()=>{
+                setWords(s);
+              })
+            }}
+          >米字格</button>
+        </div>
+        <input className="words-input"
+          defaultValue={str}
+          onChange={(e) => {
+            setWords(e.target.value)
+          }}>
+        </input>
       </header>
+      <div className="copybook-page">
+        {words.map((word, i) => <Matts type={type} size={size} key={i}>{word}</Matts>)}
+      </div>
     </div>
   );
 }
---
+++ src/matts.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+export default class Copybook extends React.Component {
+    componentDidMount() {
+        this.drawWord();
+    }
+    componentDidUpdate(oldProps) {
+        this.ctx.save();
+        const propNames = ["children", "size", 'type']
+        if (propNames.some((name) => oldProps[name] !== this.props[name])) {
+            this.drawWord();
+        }
+    }
+    drawWord() {
+        const { size } = this.props
+        this.ctx.restore();
+        this.ctx.clearRect(0, 0, size, size);
+        const { type, children: word, } = this.props;
+        switch (type) {
+            case "mi":
+                this.drawMWord();
+                break;
+            default:
+                this.drawMatts();
+        }
+        this.setWord(word);
+
+    }
+    drawMatts() {
+        const { size } = this.props
+        var ctx = this.ctx;
+        ctx.strokeStyle = "#0000ff";
+        ctx.strokeRect(1, 1, size - 2, size - 2);
+        this.ctx.setLineDash([size / 40, size / 40])
+        // ctx.strokeRect(size / 4, size / 4, size / 2, size / 2);
+        ctx.moveTo(size / 2, 0);
+        ctx.lineTo(size / 2, size);
+        ctx.stroke();
+        ctx.moveTo(0, size / 2);
+        ctx.lineTo(size, size / 2);
+        ctx.stroke();
+    }
+    drawArc() {
+        const { size } = this.props
+        this.drawMWord();
+        this.ctx.moveTo(size / 2, size / 2);
+        // this.ctx.arc(100,100,70,0,2*Math.PI);
+        this.ctx.stroke();
+    }
+    drawMWord() {
+        const { size } = this.props
+        this.drawMatts();
+        this.ctx.setLineDash([size / 40, size / 40])
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, 0);
+        this.ctx.lineTo(size, size);
+        this.ctx.stroke();
+        this.ctx.moveTo(0, size);
+        this.ctx.lineTo(size, 0);
+        this.ctx.stroke();
+    }
+    setWord(word) {
+        const { size } = this.props
+        this.ctx.textAlign = "center"
+        this.ctx.textBaseline = "middle"
+        this.ctx.font = size + "px cursive";
+        this.ctx.fillText(word, size / 2, size / 2 * 0.8);
+    }
+
+    render() {
+        const { size } = this.props
+        return <canvas height={size} width={size}
+            ref={(canvas) => {
+                if (canvas)
+                    this.ctx = canvas.getContext("2d")
+            }}
+        ></canvas >
+    }
+}
