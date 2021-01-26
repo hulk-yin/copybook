6cb5e9c483ff66da538c89b98e2bd241b0a94df7
kehao.ykh <kehao.ykh@alibaba-inc.com>
2021-01-26 22:36:56 +0800

    支持pc打印字帖

---
+++ src/components/print/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Modal } from 'antd-mobile';
+// import html2canvas from 'html2canvas';
+// import {saveAsPNG} from 'canvas2image';
+import './index.css';
+import { Button } from 'antd';
+export default (props) => {
+    const [visible, setVisible] = useState(false)
+    // const [canvas,updateCanvas] = useState(null);
+    return <div>
+        <Button
+            onClick={async () => {
+                props.onPrint()
+                await new Promise(resolve => setTimeout(resolve, 100))
+                window.print();
+                // saveAsPNG
+                window._czc && window._czc.push(["_trackEvent", "help", "click", '打印', 1, 'print_btn']);
+                // await new Promise(resolve => setTimeout(resolve, 100))
+                // const _canvas = await html2canvas(document.getElementById("copybook-page"), {
+                //     width: 1240,
+                //     height: 1757
+                // })
+                // // updateCanvas(_canvas);
+                // setVisible(true)
+                // document.getElementById("preview-page").append(_canvas);
+            }}
+        >打印</Button>
+        {/* <i
+            className="iconfont icon-print"
+            onClick={() => {
+                window._czc && window._czc.push(["_trackEvent", "help", "click", '打印', 1, 'print_btn']);
+                window.print();
+                setVisible(true)
+            }}
+        /> */}
+        <Modal
+            visible={visible}
+            onClose={() => {
+                setVisible(false)
+                props.onCancel();
+            }}
+            style={{
+                // height: "600px"
+            }}
+        > <div id="preview-page"
+            onClick={() => {
+                setVisible(false)
+                props.onCancel();
+            }}
+        ></div>
+        </Modal>
+    </div>
+}
--- src/pages/hanzi/index.tsx
+++ src/pages/hanzi/index.tsx
@@ -4,7 +4,7 @@ import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.
 import '.'
 import './index.scss';
 import Matts from '../../components/matts';
-import Help from '../../components/help/index';
+import Print from '../../components/print/index';
 declare var window: any;
 window._czc = window._czc || [];
 function App() {
@@ -13,51 +13,64 @@ function App() {
   const type = window.localStorage.getItem("setting.type") || "tian";
   const font = window.localStorage.getItem("setting.font-family") || "FZKTJW";
   const words = str.split("");
-
+  const printWords = str.split("\n").map((item: string) => {
+    const lent = item.length % 12;
+    return item + "                                                  ".slice(0, 12 - item.length % 12-1)
+  }).join("\n").split("");
+  const [isPrint, setIsPrint] = useState(false);
   return (
-    <div className="App">
-
-      <div style={{ marginTop: 45 }}>
-        <TextareaItem
-          style={{
-            border: "1px dotted #CCC",
-            padding: 5,
-            fontFamily: font,
-            maxHeight: 300,
-          }}
-          placeholder="在这里输入要生成字体的文字"
-          defaultValue={str}
-          onChange={setWords}
-          onBlur={(v) => {
-            window.localStorage.setItem("current.words", v);
-            setWords(v)
-          }}
-          autoHeight
-        />
-        <div className="copybook-page">
+    <div>
+      {isPrint ?
+        <div className="copybook-print">
           <div className="copybook-page-box" >
-            {words.map((word: string, i: number) => <Matts type={type} font={font} size={size} key={i}>{word}</Matts>)}
+            {printWords.map((word: string, i: number) => <Matts type={type} font={font} size={54} key={i}>{word}</Matts>)}
           </div>
         </div>
-        <div style={{
-          color: "#999",
-          margin: "10px",
-          marginBottom: 40
-        }}>
-          字体版权： 方正字体库(https://www.foundertype.com/)
-      </div>
-      </div>
+        : null}
 
-      <NavBar
-        style={{
-          position: "fixed",
-          top: 0,
-          width: "100%"
-        }}
-        rightContent={<Help />}
-      >汉字字帖</NavBar>
-    </div >
+      <div className="App">
 
+        <div style={{ marginTop: 45 }}>
+          <TextareaItem
+            style={{
+              border: "1px dotted #CCC",
+              padding: 5,
+              fontFamily: font,
+              maxHeight: 300,
+            }}
+            placeholder="在这里输入要生成字体的文字"
+            defaultValue={str}
+            onChange={setWords}
+            onBlur={(v) => {
+              window.localStorage.setItem("current.words", v);
+              setWords(v)
+            }}
+            autoHeight
+          />
+          <div className={"copybook-page"} id="copybook-page">
+            <div className="copybook-page-box" >
+              {words.map((word: string, i: number) => <Matts type={type} font={font} size={size} key={i}>{word}</Matts>)}
+            </div>
+          </div>
+          <div style={{
+            color: "#999",
+            margin: "10px",
+            marginBottom: 40
+          }}>
+            字体版权： 方正字体库(https://www.foundertype.com/)
+      </div>
+        </div>
+
+        <NavBar
+          style={{
+            position: "fixed",
+            top: 0,
+            width: "100%"
+          }}
+          rightContent={<Print onPrint={() => setIsPrint(true)} onCancel={() => setIsPrint(false)} />}
+        >汉字字帖</NavBar>
+      </div >
+    </div>
   );
 }
 
